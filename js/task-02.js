a42d239c4759e64be01cbec124cf91598b42e212
RonnieBk <veronicabrvk00@gmail.com>
2023-09-06 00:27:06 +0200

    Update task 02 js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfIngredients = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const element = document.createElement("li");
+  element.classList.add("item");
+  element.textContent = ingredient;
+  listOfIngredients.append(element);
+});
