7e4eb21d7344dbf7696d67ad6ea6e42be319a735
RonnieBk <veronicabrvk00@gmail.com>
2023-09-06 21:09:29 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log({
+    Email: email,
+    Password: password,
+  });
+  form.reset();
+}
