9b94856dbba76a3e9b2d1e6e1189f7f875b8bf3a
RonnieBk <veronicabrvk00@gmail.com>
2023-09-06 17:00:27 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+let counterValue = 0;
+const result = document.querySelector("#value");
+const btnDecrement = document.querySelector("button[data-action='decrement']");
+const btnIncrement = document.querySelector("button[data-action='increment']");
+
+btnDecrement.addEventListener("click", () => {
+  counterValue -= 1;
+  result.textContent = counterValue;
+});
+
+btnIncrement.addEventListener("click", () => {
+  counterValue += 1;
+  result.textContent = counterValue;
+});
