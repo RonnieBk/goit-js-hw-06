27d40e37dbc8470bf6423b2095e793581a4f4d9b
RonnieBk <veronicabrvk00@gmail.com>
2023-09-07 13:50:18 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,9 @@
 const categoriesList = document.querySelector("#categories");
 const allCategories = categoriesList.querySelectorAll(".item");
 
-const firstCategory = categoriesList.firstElementChild;
-const secondCategory = firstCategory.nextElementSibling;
-const thirdCategory = categoriesList.lastElementChild;
+console.log("Number of categories:", allCategories.length);
 
-const firstCategoryName = firstCategory.firstElementChild.textContent;
-const secondCategoryName = secondCategory.firstElementChild.textContent;
-const thirdCategoryName = thirdCategory.firstElementChild.textContent;
-
-const firstCategoryItems = firstCategory.lastElementChild.children;
-const secondCategoryItems = secondCategory.lastElementChild.children;
-const thirdCategoryItems = thirdCategory.lastElementChild.children;
-
-console.log(`Number of categories: ${allCategories.length}`);
-
-console.log(`Category: ${firstCategoryName}`);
-console.log(`Elements: ${firstCategoryItems.length}`);
-
-console.log(`Category: ${secondCategoryName}`);
-console.log(`Elements: ${secondCategoryItems.length}`);
-
-console.log(`Category: ${thirdCategoryName}`);
-console.log(`Elements: ${thirdCategoryItems.length}`);
+allCategories.forEach((category) => {
+  console.log("Category:", category.firstElementChild.textContent);
+  console.log("Elements:", category.lastElementChild.children.length);
+});
